fc5027cfc9153d2ef41b5867cc879ee85101e143
Anggel Romero <mihaelloro@gmail.com>
2020-03-24 18:27:04 -0500

    delete files node_modules

---
+++ por-hacer/por-hacer.js
@@ -0,0 +1,95 @@
+
+
+const fs = require('fs');
+
+let ListadoPorHacer = [];
+
+
+const  guardarDB = () => {
+       
+    let data = JSON.stringify(ListadoPorHacer);
+
+    fs.writeFile('db/data.json', data, (err)=> {
+        if ( err) throw new Error('No se pudo grabar', err);
+
+    });
+
+
+}
+
+
+const cargarDB = () => {
+
+    try {
+
+        ListadoPorHacer = require('../db/data.json');
+        
+    } catch (error) {
+        ListadoPorHacer = [];
+    }
+
+
+}
+
+
+const crear = (descripcion) => {
+
+    cargarDB();
+
+    let porHacer = {
+        descripcion,
+        completado: false
+    };
+
+    ListadoPorHacer.push(porHacer);
+
+    guardarDB();
+
+    return porHacer;
+
+}
+
+const getListado = () => {
+    cargarDB();
+    return ListadoPorHacer;
+
+}
+
+const actualizar = (descripcion, completado = true) => {
+
+    cargarDB();
+
+    let index = ListadoPorHacer.findIndex( tarea => tarea.descripcion === descripcion);
+
+    if ( index >= 0 ) {
+        ListadoPorHacer[index].completado = completado;
+        guardarDB();
+        return true;
+    }else {
+        return false;
+    }
+}
+
+const borrar = (descripcion) => {
+
+    cargarDB();
+    
+    let nuevoListado = ListadoPorHacer.filter(tarea => tarea.descripcion !== descripcion);
+    
+    if ( ListadoPorHacer.length === nuevoListado.length ) {
+        return false;
+    }else {
+        ListadoPorHacer = nuevoListado;
+        guardarDB();
+        return true;
+    }
+
+}
+
+
+module.exports = {
+    crear,
+    getListado,
+    actualizar,
+    borrar
+}
